a0dc3aebd0df0e5ac13a6e5fb2c55e6ba075dfbd
Bhim Timsina <bhimtimsiana63@gmail.com>
2026-02-04 19:33:19 +0545

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,269 @@
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets, exceptions, status
+from rest_framework.response import Response
+from api.permissions import IsStaffOrOwner
+
+from api.serializers import (
+    CategorySerializer,
+    CommentSerializer,
+    ContactSerializer,
+    GroupSerializer,
+    NewsletterSerializer,
+    PostPublishSerializer,
+    PostSerializer,
+    TagSerializer,
+    UserSerializer,
+    UserRegistrationSerializer,
+)
+from newspaper.models import Category, Comment, Contact, Newsletter, Post, Tag
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+
+
+# CRUD
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+
+    queryset = Group.objects.all().order_by("name")
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Tags to be viewed or edited.
+    """
+
+    queryset = Tag.objects.all().order_by("name")
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [permissions.AllowAny()]
+
+        return super().get_permissions()
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Categories to be viewed or edited.
+    """
+
+    queryset = Category.objects.all().order_by("name")
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAdminUser]
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [permissions.AllowAny()]
+
+        return super().get_permissions()
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Posts to be viewed or edited.
+    """
+
+    queryset = Post.objects.all().order_by("-published_at")
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.action in ["list", "retrieve"]:
+            queryset = queryset.filter(status="active", published_at__isnull=False)
+
+            # search start:
+            from django.db.models import Q
+
+            query = self.request.query_params.get("query", None)
+            if query:
+                # Search by title and content (case-insensitive)
+                queryset = queryset.filter(
+                    Q(title__icontains=query) | Q(content__icontains=query)
+                )
+            # search end
+        return queryset
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            return [permissions.AllowAny()]
+        return super().get_permissions()
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.views_count += 1  # Increment the views_count
+        instance.save(update_fields=["views_count"])  # Save only the updated field
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
+
+
+class PostListByCategoryView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(
+            status="active",
+            published_at__isnull=False,
+            category=self.kwargs["category_id"],
+        )
+        return queryset
+
+
+class PostListByTagView(ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(
+            status="active",
+            published_at__isnull=False,
+            tag=self.kwargs["tag_id"],
+        )
+        return queryset
+
+
+class DraftListView(ListAPIView):
+    queryset = Post.objects.filter(published_at__isnull=True)
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+class DraftDetailView(RetrieveAPIView):
+    queryset = Post.objects.filter(published_at__isnull=True)
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+
+from rest_framework import exceptions
+
+class NewsletterViewSet(viewsets.ModelViewSet):
+    queryset = Newsletter.objects.all()
+    serializer_class = NewsletterSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve", "destroy"]:
+            return [permissions.IsAdminUser()]
+        return super().get_permissions()
+
+    def update(self, request, *args, **kwargs):
+        raise exceptions.MethodNotAllowed(request.method)
+
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve", "destroy"]:
+            return [permissions.IsAdminUser()]
+        return super().get_permissions()
+
+    def update(self, request, *args, **kwargs):
+        raise exceptions.MethodNotAllowed(request.method)
+
+
+from rest_framework.views import APIView
+from rest_framework import status
+from django.utils import timezone
+
+
+class PostPublishViewSet(APIView):
+    permission_classes = [permissions.IsAdminUser]
+
+    def post(self, request, *args, **kwargs):
+        serializer = PostPublishSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            data = serializer.data
+
+            # publish the post
+            post = Post.objects.get(pk=data["id"])
+            post.published_at = timezone.now()
+            post.save()
+
+            serialized_data = PostSerializer(post).data
+            return Response(serialized_data, status=status.HTTP_200_OK)
+
+
+class CommentListCreateAPIView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get_permissions(self):
+        if self.request.method == "POST":
+            return [permissions.IsAuthenticated()]
+        return [permissions.AllowAny()]
+
+    def get(self, request, post_id, *args, **kwargs):
+        comments = Comment.objects.filter(post=post_id).order_by("-created_at")
+        serialized_data = CommentSerializer(comments, many=True).data
+        return Response(serialized_data, status=status.HTTP_200_OK)
+
+    def post(self, request, post_id, *args, **kwargs):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save(post_id=post_id, user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class CommentDetailAPIView(APIView):
+    permission_classes = [IsStaffOrOwner]
+
+    def get_object(self, pk):
+        try:
+            obj = Comment.objects.get(pk=pk)
+            self.check_object_permissions(self.request, obj)
+            return obj
+        except Comment.DoesNotExist:
+            raise exceptions.NotFound({"detail": "Comment not found."})
+
+    def get(self, request, post_id, pk, *args, **kwargs):
+        comment = self.get_object(pk)
+        serialized_data = CommentSerializer(comment).data
+        return Response(serialized_data, status=status.HTTP_200_OK)
+
+    def put(self, request, post_id, pk, *args, **kwargs):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment, data=request.data, partial=False)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def patch(self, request, post_id, pk, *args, **kwargs):
+        comment = self.get_object(pk)
+        serializer = CommentSerializer(comment, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, post_id, pk, *args, **kwargs):
+        comment = self.get_object(pk)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+from rest_framework.generics import CreateAPIView
+
+
+class UserRegistrationView(CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    permission_classes = [permissions.AllowAny]
---
+++ newspaper/models.py
@@ -0,0 +1,163 @@
+from django.db import models
+
+
+class TimeStampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True  # Don't create table in DB
+
+
+class Category(TimeStampModel):
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]  # Category.objects.all()
+        verbose_name = "category"
+        verbose_name_plural = "Categories" 
+
+
+class Tag(TimeStampModel):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(TimeStampModel):
+    STATUS_CHOICES = [
+        ("active", "Active"),
+        ("in_active", "Inactive"),
+    ]
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    featured_image = models.ImageField(upload_to="post_images/%Y/%m/%d", blank=False)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="active")
+    views_count = models.PositiveBigIntegerField(default=0)
+    is_breaking_news = models.BooleanField(default=False)
+    published_at = models.DateTimeField(null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tag = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+
+class Advertisement(TimeStampModel):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="advertisements/%Y/%m/%d", blank=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Contact(TimeStampModel):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["created_at"]  # Contact.objects.all() => order_by("created_at")
+        # db_table = 'contact'
+
+
+# post.author.userprofile.image
+class UserProfile(TimeStampModel):
+    user = models.OneToOneField("auth.User", on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="user_images/%Y/%m/%d", blank=False)
+    address = models.CharField(max_length=200)
+    biography = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
+
+# post.comment_set.all()
+class Comment(TimeStampModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"{self.content[:50]} | {self.user.username}"
+
+
+class Newsletter(TimeStampModel):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+
+class OurTeam(TimeStampModel):
+    name = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="team_images/%Y/%m/%d", blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+# post - comment
+# 1 post can have M comments => M
+# 1 comment is associated to only 1 post => 1
+# ForeignKey => M
+
+# comment-user
+# 1 user can add M comment => M
+# 1 comment is associated to only 1 user => 1
+
+# Post - Author
+# 1 author can add M posts => M
+# 1 post is associated to only 1 author => 1
+
+
+# 1 category can have M posts => M
+# 1 post is associated to only 1 category => 1
+
+
+# Tag - Post
+# 1 tag can have M posts => M
+# 1 post can have M tags => M
+
+
+## 1 - 1 Relationship
+# 1 user can have 1 profile => 1
+# 1 profile is associated to 1 user  => 1
+# OneToOneField() => Can be used in Any Model
+
+
+## 1 - M Relationship
+# 1 user can post M post  => M
+# 1 post is associated to only 1 user => 1
+# In django use ForeignKey() => Must be used in Many side
+
+# 1 category can have M posts => M
+# 1 post is associated to only 1 category => 1
+
+
+# 1 tag can have M posts => M
+# 1 post can have M tags => M
+
+
+## M - M Relationship
+# 1 student can learn from M teachers => M
+# 1 teacher can teach M students => M
+# ManyToManyField() => Can be used in Any Model
+
+# 1 post can contain M tag => M
+# 1 tag can be used in M post => M
+
+# 1 post can be associated to only 1 category => 1
+# 1 category can contains M post => M
